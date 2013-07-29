e78677283cc06be606da8d41e74beb3fa38cb916
Zhi Yong Wu <wuzhy@linux.vnet.ibm.com>
2013-07-29 23:32:18 +0800

    VFS hot tracking: Add an ioctl to get hot tracking information
    FS_IOC_GET_HEAT_INFO: return a struct containing the various
    metrics collected in hot_freq_data structs, and also return a
    calculated data temperature based on those metrics.
    
    Optionally, retrieve the temperature from the hot data hash list
    instead of recalculating it.
    
    Signed-off-by: Chandra Seetharaman <sekharan@us.ibm.com>
    Signed-off-by: Zhi Yong Wu <wuzhy@linux.vnet.ibm.com>

--- fs/compat_ioctl.c
+++ fs/compat_ioctl.c
@@ -57,6 +57,7 @@
 #include <linux/i2c-dev.h>
 #include <linux/atalk.h>
 #include <linux/gfp.h>
+#include <linux/hot_tracking.h>
 
 #include <net/bluetooth/bluetooth.h>
 #include <net/bluetooth/hci_sock.h>
@@ -1400,6 +1401,9 @@ COMPATIBLE_IOCTL(TIOCSTART)
 COMPATIBLE_IOCTL(TIOCSTOP)
 #endif
 
+/*Hot data tracking*/
+COMPATIBLE_IOCTL(FS_IOC_GET_HEAT_INFO)
+
 /* fat 'r' ioctls. These are handled by fat with ->compat_ioctl,
    but we don't want warnings on other file systems. So declare
    them as compatible here. */
@@ -1580,6 +1584,7 @@ COMPAT_SYSCALL_DEFINE3(ioctl, unsigned int, fd, unsigned int, cmd,
 	case FIBMAP:
 	case FIGETBSZ:
 	case FIONREAD:
+	case FS_IOC_GET_HEAT_INFO:
 		if (S_ISREG(file_inode(f.file)->i_mode))
 			break;
 		/*FALL THROUGH*/
